5a1debb2bc4247258201109f63c47fd204a39b2b
ahonestla <vicozer@gmail.com>
2025-11-04 16:38:36 +0100

    feat(backend): add ovhai objects routes

--- backend/app/hf.py
+++ backend/app/hf.py
@@ -9,9 +9,12 @@ router = APIRouter(prefix="/hf")
 
 @router.get("/datasets")
 def hf_list_datasets(owner: str = None, limit: int = 100):
-    datasets = list_datasets(author=owner, limit=limit)
-    datasets = [dataset.__dict__ for dataset in datasets]
-    return datasets
+    try:
+        datasets = list_datasets(author=owner, limit=limit)
+        datasets = [dataset.__dict__ for dataset in datasets]
+        return datasets
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
 
 
 @router.get("/datasets/{owner}/{name}")
@@ -25,10 +28,12 @@ async def hf_get_dataset(owner: str, name: str):
 
 @router.get("/models")
 def hf_list_models(owner: str = None, limit: int = 100):
-    models = list_models(author=owner, limit=limit, fetch_config=True)
-    models = [model.__dict__ for model in models]
-    return models
-
+    try:
+        models = list_models(author=owner, limit=limit, fetch_config=True)
+        models = [model.__dict__ for model in models]
+        return models
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
 
 @router.get("/models/{owner}/{name}")
 async def hf_get_model(owner: str, name: str):
--- backend/app/ovhai.py
+++ backend/app/ovhai.py
@@ -1,14 +1,16 @@
 import os
 import json
 import subprocess
-from typing import Literal
-from fastapi import APIRouter
+from typing import Literal, Annotated
+from fastapi import APIRouter, File, HTTPException
 from app.logger import get_logger
 from app.ovhai_finetuning import JOB as JOB_FT
 
 logger = get_logger(__name__)
 
 router = APIRouter(prefix="/ovhai")
+
+DATA_STORE = "1azgra"
 
 JOB_STATE = Literal[
     "QUEUED",
@@ -28,61 +30,124 @@ JOB_STATE = Literal[
 JOB_ACTIONS = Literal["GET", "STOP"]
 
 def ovhai_initialize():
+    # login
     cmd = f'ovhai login --username {os.getenv("OVHAI_USERNAME")} --password-from-env OVHAI_PASSWORD'
     result = subprocess.run(cmd, shell=True, text=True)
     result.check_returncode()
 
+    # add s3 datastore
+    cmd = f'ovhai datastore update s3 {DATA_STORE} {os.getenv("OVHAI_OS_ENDPOINT")} {os.getenv("OVHAI_OS_REGION").lower()} {os.getenv("OVHAI_OS_ACCESS_KEY")} --secret-key-from-env OVHAI_OS_SECRET_KEY --store-credentials-locally'
+    result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+    result.check_returncode()
 
-def cmd_get_data(cmd: str) -> dict:
-    data = {}
-    try:
-        result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
-        result.check_returncode()
-        data = json.loads(result.stdout)
-    except:
-        logger.debug(f"error getting data for cmd {cmd}")
-    return data
+
+def cmd_run(cmd: str, capture_json: bool = False):
+    result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+
+    if result.returncode != 0:
+        logger.error(f"CMD ERR: {result.stderr}")
+        raise Exception(f"CMD ERR: {result.stderr}")
+
+    if result.returncode == 0 and capture_json:
+        try:
+            data: dict = json.loads(result.stdout)
+            return data
+        except Exception:
+            logger.error(f"Error while parsing json from {result.stdout}")
+            raise ValueError(f"Error while parsing json from {result.stdout}")
+
+
+def ovhai_object_upload(file_path: str, container: str, prefix: str = None):
+    cmd = f"ovhai bucket object upload {container}@{DATA_STORE} {file_path}"
+    if prefix:
+        cmd += f" --add-prefix {prefix}"
+    cmd_run(cmd)
+
+
+def ovhai_object_delete(object_name: str, container: str):
+    cmd = f"ovhai bucket object delete {container}@{DATA_STORE}"
+    cmd_run(cmd)
+
 
 def ovhai_job_run(job_cli: str):
     cmd = f"ovhai job run -o json {job_cli}"
-    data = cmd_get_data(cmd)
+    data = cmd_run(cmd, capture_json=True)
     return data
 
 
 def ovhai_job_stop(id: str):
     cmd = f"ovhai job stop {id}"
-    result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
-    result.check_returncode()
+    cmd_run(cmd)
 
 
 def ovhai_job_get(id: str):
     cmd = f"ovhai job get {id} -o json"
-    data = cmd_get_data(cmd)
+    data = cmd_run(cmd, capture_json=True)
     return data
+
+
+@router.get("/objects/{container}")
+def objects_list(container: str):
+    cmd = f"ovhai bucket object list -o json {container}@{DATA_STORE}"
+    try:
+        data = cmd_run(cmd, capture_json=True)
+        return data
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
+
+
+@router.post("/objects/upload_json")
+def objects_upload_json(content: dict, file_path: str, container: str, prefix: str = None):
+    tmp_path = f"/tmp/{file_path}"
+    if not tmp_path.endswith(".json"):
+        tmp_path += ".json"
+    with open(tmp_path, "w") as f:
+        json.dump(content, f)
+    try:
+        ovhai_object_upload(tmp_path, container, prefix)
+        os.remove(tmp_path)
+        return {f"{file_path}": "uploaded"}
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
+
+
+@router.post("/objects/delete")
+def objects_delete(object_name: str, container: str):
+    try:
+        ovhai_object_delete(object_name, container)
+        return {f"{object_name}": "deleted"}
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=str(error))
 
 
 @router.get("/jobs")
-def ovhai_job_list(state: JOB_STATE = None):
+def jobs_list(state: JOB_STATE = None):
     filter = f"-s {state}" if state else "-a"
     cmd = f"ovhai job list -o json {filter}"
-    data = cmd_get_data(cmd)
-    return data
+    try:
+        data = cmd_run(cmd, capture_json=True)
+        return data
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
 
 
 @router.get("/jobs/{id}")
-async def ovhai_job(id: str, action: JOB_ACTIONS = "GET"):
-    if action == "GET":
-        data = ovhai_job_get(id)
-        return data
-    if action == "STOP":
-        try:
+async def jobs_action(id: str, action: JOB_ACTIONS = "GET"):
+    try:
+        if action == "GET":
+            data = ovhai_job_get(id)
+            return data
+        if action == "STOP":
             ovhai_job_stop(id)
-            return {"message": "ok"}
-        except Exception as error:
-            return {"error": error.stderr}
+            return {f"{id}": "stopped"}
+    except Exception as error:
+        raise HTTPException(status_code=404, detail=str(error))
 
 
 @router.post("/jobs/finetuning")
-async def ovhai_finetune(job: JOB_FT):
-    data = ovhai_job_run(job.get_cli())
-    return data
+async def jobs_finetune(job: JOB_FT):
+    try:
+        data = ovhai_job_run(job.get_cli())
+        return data
+    except Exception as error:
+        raise HTTPException(status_code=400, detail=str(error))
